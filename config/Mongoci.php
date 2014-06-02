69de724ac772b7e663e0366033a016ef9830891e
Muhammad Mohsin Ali Khan <x2mohsinali@hotmail.com>
2014-06-02 16:31:14 +0500

    First commit.

---
+++ config/Mongoci.php
@@ -0,0 +1,22 @@
+<?php
+
+	//whether username and password is required to access mongodb
+	$config['auth'] = true;
+
+	//enter your mongodb user name
+	$config['user'] = 'root';
+	
+	//mongodb password
+	$config['password'] = 'bitnami';
+	
+	//mongodb database name
+	$config['db_name'] = 'admin';
+	
+	//ip address where mongodb is hosted
+	$config['host'] = 'localhost';
+	
+	//the port being used by mongodb (default 27017)
+	$config['port'] = '27017';
+	
+
+?>
